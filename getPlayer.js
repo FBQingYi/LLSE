1867e3eb941813effb19218c07af459f633944e1
清漪花开 <82281127+FBQingYi@users.noreply.github.com>
2022-03-24 19:34:49 +0800

    Update getPlayer.js

--- getPlayer.js
+++ getPlayer.js
@@ -1,4 +1,12 @@
-let playerList = mc.getOnlinePlayers();
-playerList.forEach((player) => {
-    log(player)
+mc.listen("onServerStarted", () => {
+    let Command = mc.newCommand('c', '测试指令！', PermType.Any)
+    let playerListName = "";
+    Command.setCallback((cmd, origin, output, results) => {
+        let playerList = mc.getOnlinePlayers();
+        playerList.forEach((player) => {
+            playerListName += player.name + '\n';
+        })
+    })
+    outp.success(playerListName);
+    Command.setup();
 })
