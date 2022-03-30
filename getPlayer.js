80c49c45c6a30f4e86c10721e401532a187275c3
userguo990104 <56616026+userguo990104@users.noreply.github.com>
2022-03-30 12:09:31 +0800

    Update getPlayer.js
    测试

--- getPlayer.js
+++ getPlayer.js
@@ -1,4 +1,5 @@
 mc.listen("onServerStarted", () => {
+    //秃头guo修改
     let Command = mc.newCommand('c', '测试指令！', PermType.Any)
     let playerListName = "";
     Command.setCallback((cmd, origin, output, results) => {
